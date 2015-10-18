634c5dc3aa6653f260a06ef7092165f5d3654094
Hans Ott <hansott@hotmail.be>
2015-10-18 15:43:29 +0200

    Cleanup software file.

--- software.sh
+++ software.sh
@@ -20,27 +20,46 @@ brew update
 # Upgrade any already-installed formulae.
 brew upgrade --all
 
-# Install GNU core utilities (those that come with OS X are outdated).
-# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
-brew install coreutils
-brew install findutils
-brew install bash
-sudo ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
+#
+# Given a list of desired items and installed items, return a list
+# of uninstalled items. Arrays in bash are insane (not in a good way).
+# Credit: https://github.com/cowboy/dotfiles
+#
+function to_install() {
+    local debug desired installed i desired_s installed_s remain
+    if [[ "$1" == 1 ]]; then debug=1; shift; fi
+        # Convert args to arrays, handling both space- and newline-separated lists.
+        read -ra desired < <(echo "$1" | tr '\n' ' ')
+        read -ra installed < <(echo "$2" | tr '\n' ' ')
+        # Sort desired and installed arrays.
+        unset i; while read -r; do desired_s[i++]=$REPLY; done < <(
+            printf "%s\n" "${desired[@]}" | sort
+        )
+        unset i; while read -r; do installed_s[i++]=$REPLY; done < <(
+            printf "%s\n" "${installed[@]}" | sort
+        )
+        # Get the difference. comm is awesome.
+        unset i; while read -r; do remain[i++]=$REPLY; done < <(
+            comm -13 <(printf "%s\n" "${installed_s[@]}") <(printf "%s\n" "${desired_s[@]}")
+        )
+    [[ "$debug" ]] && for v in desired desired_s installed installed_s remain; do
+        echo "$v ($(eval echo "\${#$v[*]}")) $(eval echo "\${$v[*]}")"
+    done
+    echo "${remain[@]}"
+}
 
-# Install more recent versions of some OS X tools
-brew tap homebrew/dupes
-brew install homebrew/dupes/grep
-
-binaries=(
-    wget --with-iri
-    imagemagick --with-webp
+#
+# The homebrew packages to install.
+#
+packages=(
+    wget
+    imagemagick
     graphicsmagick
     webkit2png
     rename
     httpie
     ffmpeg
     python
-    sshfs
     trash
     node
     tree
@@ -54,14 +73,26 @@ binaries=(
     carthage
 )
 
-echo "Installing binaries..."
-brew install ${binaries[@]}
+echo "Installing homebrew packages..."
+list="$(to_install "${packages[*]}" "$(brew list)")"
+if [[ "$list" ]]; then
+    for item in ${list[@]}
+        do
+            echo "$item is not installed."
+            brew install $item
+        done
+else
+    echo "All homebrew packages are installed."
+fi
 
-echo "Installing Homebrew cask..."
-brew install caskroom/cask/brew-cask
-
-# Apps
-apps=(
+#
+# Homebrew casks
+#
+if test ! $(which brew); then
+    echo "Installing homebrew cask..."
+    brew install caskroom/cask/brew-cask
+fi
+casks=(
     alfred
     dropbox
     google-chrome
@@ -87,11 +118,20 @@ apps=(
     spectacle
     caffeine
 )
-
-# Install apps to /Applications
-echo "Installing applications..."
-brew cask install --appdir=/Applications ${apps[@]}
+# Install casks to /Applications
+echo "Installing homebrew casks..."
+list="$(to_install "${casks[*]}" "$(brew cask list)")"
+if [[ "$list" ]]; then
+    for item in ${list[@]}
+        do
+            echo "Cask $item is not installed."
+            brew cask install --appdir=/Applications $item
+        done
+else
+    echo "All homebrew casks are installed."
+fi
 
 # Cleanup
 brew cleanup
-echo "That's it folks!"
+brew-cask cleanup
+echo "Done."
